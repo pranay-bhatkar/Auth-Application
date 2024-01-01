462b197e329472c87bf1d6bedaa51387ce041924
pranay-bhatkar <pranaybhatkar81@gmail.com>
2024-01-02 04:23:53 +0530

    header updated

--- components/auth/header.tsx
+++ components/auth/header.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
 import { Poppins } from "next/font/google";
-
+import { FaUserLock } from "react-icons/fa6";
 
 const font = Poppins ({
     subsets: ["latin"],
@@ -18,7 +18,9 @@ export  const Header =({
 }:HeaderProps)=> {
     return (
         <div className="w-full flex flex-col gap-y-4 items-center justify-center">
-            <h1 className={cn("text-3xl font-semibold",font.className)}>🔐 Auth</h1>
+            <h1 className={cn("text-3xl font-semibold flex gap-x-2 border-b-2 border-gray-700 rounded-md p-1 shadow-lg shadow-gray-400",font.className)}>
+                <FaUserLock className='text-yellow-300'/>
+                Auth</h1>
             <p className="text-muted-foreground text-sm">{label}</p>
         </div>
     )
